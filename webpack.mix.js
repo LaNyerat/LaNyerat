bba09a65433398da4928bd0f76ca697306469e6a
alfanjauhari <hi.alfanj@gmail.com>
2020-03-16 08:41:59 +0700

    Add Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,28 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'public/ln-content/themes/miniblog/assets/css/bootstrap.min.css',
+    'public/ln-content/themes/miniblog/assets/css/magnific-popup.css',
+    'public/ln-content/themes/miniblog/assets/css/jquery-ui.css',
+    'public/ln-content/themes/miniblog/assets/css/owl.carousel.min.css',
+    'public/ln-content/themes/miniblog/assets/css/owl.theme.default.min.css',
+    'public/ln-content/themes/miniblog/assets/css/bootstrap-datepicker.css',
+    'public/ln-content/themes/miniblog/assets/fonts/flaticon/font/flaticon.css',
+    'public/ln-content/themes/miniblog/assets/css/aos.css',
+    'public/ln-content/themes/miniblog/assets/css/style.css'
+], 'public/css/frontend/main.min.css')
+.scripts([
+    'public/ln-content/themes/miniblog/assets/js/jquery-3.3.1.min.js',
+    'public/ln-content/themes/miniblog/assets/js/jquery-migrate-3.0.1.min.js',
+    'public/ln-content/themes/miniblog/assets/js/jquery-ui.js',
+    'public/ln-content/themes/miniblog/assets/js/popper.min.js',
+    'public/ln-content/themes/miniblog/assets/js/bootstrap.min.js',
+    'public/ln-content/themes/miniblog/assets/js/owl.carousel.min.js',
+    'public/ln-content/themes/miniblog/assets/js/jquery.stellar.min.js',
+    'public/ln-content/themes/miniblog/assets/js/jquery.countdown.min.js',
+    'public/ln-content/themes/miniblog/assets/js/jquery.magnific-popup.min.js',
+    'public/ln-content/themes/miniblog/assets/js/bootstrap-datepicker.min.js',
+    'public/ln-content/themes/miniblog/assets/js/aos.js',
+    'public/ln-content/themes/miniblog/assets/js/main.js'
+], 'public/js/frontend/main.min.js').version();
