f3cd17de4d17dd6a6c96a34880b37dc241ce7855
alfanjauhari <hi.alfanj@gmail.com>
2020-03-16 11:12:01 +0700

    Delete Unused Comments

--- resources/views/miniblog/layouts/app.blade.php
+++ resources/views/miniblog/layouts/app.blade.php
@@ -16,19 +16,6 @@
 			@yield('content')
 		</div>
 
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery-3.3.1.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery-migrate-3.0.1.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery-ui.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/popper.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/bootstrap.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/owl.carousel.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery.stellar.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery.countdown.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/jquery.magnific-popup.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/bootstrap-datepicker.min.js') }}"></script> --}}
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/aos.js') }}"></script> --}}
-
-  {{-- <script src="{{ asset('ln-content/themes/miniblog/assets/js/main.js') }}"></script> --}}
 	<script src="{{ asset('js/frontend/main.min.js') }}"></script>
 </body>
 </html>
